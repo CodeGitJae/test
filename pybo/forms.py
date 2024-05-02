ced6c9e905f4a9475f5e7ca601faa5127911c9f0
mbc <dnsrldl123@google.com>
2024-05-02 16:24:46 +0900

    회원정보

---
+++ pybo/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, EmailField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class UserCreateForm(FlaskForm):
+    username = StringField('아이디', validators=[DataRequired(), Length(min=5, max=20)])
+    password1 = PasswordField('비밀번호', validators=[DataRequired(), EqualTo("password2", "비밀번호가 일치하지 않음")])
+    password2 = PasswordField('비밀번호 확인', validators=[DataRequired()])
+    email = EmailField("이메일", validators=[DataRequired(), Email()])
+
+class UserLoginForm(FlaskForm):
+    username = StringField('아이디', validators=[DataRequired(), Length(min=3, max=20)])
+    password = PasswordField('비밀번호', validators=[DataRequired()])
--- pybo/views/auth_views.py
+++ pybo/views/auth_views.py
@@ -1,7 +1,98 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template,request, flash, session, url_for,g
+from pybo import db
+from pybo.forms import UserCreateForm, UserLoginForm
+from pybo.models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import redirect
+import functools
+
+
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
-@bp.route("/login")
+@bp.route("/checkusername/<username>")
+def check_user_name(username):
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        return str(1)
+    else:
+        return str(-1)
+
+
+
+
+def required_login(func):
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if g.user is None:
+            to = request.url if request.method=='GET' else ''
+
+            return redirect(url_for('auth.login'))
+        
+        return func(*args, **kwargs)
+    
+    return wrapper
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("main.index"))
+
+
+@bp.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("auth/login.html")
+    form = UserLoginForm()
+
+
+    if request.method == "POST" and form.validate_on_submit():
+
+        error = None
+
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            error = "존재하지 않는 회원입니다"
+
+        elif not check_password_hash(user.password, form.password.data):
+            error = "비밀번호가 틀렸습니다"
+
+        if error is None:
+            print("bbbbbbbbbbbbbbbbbbbb")
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for("main.index"))
+        
+        flash(error)
+
+
+    return render_template("auth/login.html", form=form)
+
+@bp.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = UserCreateForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        user2 = User.query.filter_by(email=form.email.data).first()
+
+        if user2:
+            flash("중복된 이메일입니다")
+            return render_template("auth/signup.html", form=form)
+
+        if not user:
+            username = form.username.data
+            password = generate_password_hash(form.password1.data)
+            email = form.email.data
+
+            user = User(username=username, password=password, email=email)
+
+            db.session.add(user)
+            db.session.commit()
+
+            return redirect(url_for("main.index"))
+        
+        else:
+            flash("중복된 아이디입니다")
+
+    return render_template("auth/signup.html", form=form)
